aa0c5e6fc38c2a3fb190f1e14f994e77cb4ef8c7
Ridote <daniridao@gmail.com>
2018-08-19 20:42:50 +0100

    Animation not working yet

--- Entities/Player/Player.gd
+++ Entities/Player/Player.gd
@@ -2,6 +2,16 @@ extends KinematicBody2D
 
 export var speed = 300
 
+#Control
+#Last direction to set up an iddle animation > 0 Up / 1 Down / 2 Right / 3 Left
+enum LAST_DIRECTION{
+ 	UP,
+ 	DOWN,
+	RIGHT,
+	LEFT
+}
+var lastDirection = LAST_DIRECTION.UP
+
 #Input
 var skill1 = false
 var skill2 = false
@@ -30,6 +40,31 @@ func readInput():
 	
 func move(delta):
 	var speedDirection = Vector2(int(moveRight) + (int(moveLeft)*-1), int(moveDown) + (int(moveUp)*-1)).normalized()
+	animate(speedDirection)
 	#move_and_collide <- needs delta
 	#move_and_slide <- doesn't need delta
 	move_and_slide(speedDirection*speed)
+
+func animate(speedDirection):
+	if speedDirection.x > 0.1:
+		$AnimationPlayer.play("WalkRight")
+		lastDirection = LAST_DIRECTION.RIGHT
+	elif speedDirection.x < -0.1:
+		$AnimationPlayer.play("WalkLeft")
+		lastDirection = LAST_DIRECTION.LEFT
+	elif speedDirection.y < -0.1:
+		$AnimationPlayer.play("WalkUp")
+		lastDirection = LAST_DIRECTION.UP
+	elif speedDirection.y > 0.1:
+		$AnimationPlayer.play("WalkDown")
+		lastDirection = LAST_DIRECTION.DOWN
+	else:
+		if lastDirection == LAST_DIRECTION.UP:
+			pass
+		elif lastDirection == LAST_DIRECTION.DOWN:
+			pass
+		elif lastDirection == LAST_DIRECTION.LEFT:
+			pass
+		else:
+			pass
+			
