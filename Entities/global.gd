c9ab9aa01299b651eb8446dc5d8364f9169e278f
Jairo <kidandcat@gmail.com>
2018-09-01 20:36:54 +0200

    improve timer

--- Entities/global.gd
+++ Entities/global.gd
@@ -5,7 +5,7 @@ var questWelcome = preload("res://Entities/GUI/QuestWelcome.tscn")
 export var playerHP = 0
 
 func _ready():
-	utils.newTimer(2, self, "showQuest")
+	utils.newTimer(2, self, "showQuest", false)
 
 func showQuest():
 	var q = questWelcome.instance()
--- Entities/utils.gd
+++ Entities/utils.gd
@@ -1,10 +1,11 @@
 extends Node
 
 
-func newTimer(time, functionOwner, function):
+func newTimer(time, functionOwner, function, repeat):
 	var timer = Timer.new()
 	timer.set_wait_time(time)
-	timer.one_shot = true
+	if not repeat:
+		timer.one_shot = true
 	timer.connect("timeout",functionOwner,function) 
 	add_child(timer)
 	timer.start()
