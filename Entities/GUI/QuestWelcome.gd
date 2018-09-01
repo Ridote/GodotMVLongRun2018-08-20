b2149181006d9967aa116cbb4a9d5756ba082c37
Jairo <kidandcat@gmail.com>
2018-09-01 12:24:50 +0200

    minimal GUI

---
+++ Entities/GUI/QuestWelcome.gd
@@ -0,0 +1,12 @@
+extends CanvasLayer
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	print("quest readdyyy")
+
+func show():
+	print("Let's Go!!")
+	#self.hide()
