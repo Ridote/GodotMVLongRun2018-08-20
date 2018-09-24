1cf802b52c287900c03cb27c24af2d76acf88669
Ridote <daniridao@gmail.com>
2018-09-24 18:36:23 +0100

    EvilBlock back to kinematic. We need to fix moving them with the player.

--- Entities/Enemies/EvilBlock/EvilBlock.gd
+++ Entities/Enemies/EvilBlock/EvilBlock.gd
@@ -21,17 +21,12 @@ func _ready():
 			direction = utils.DIRECTION.DOWN
 
 func _physics_process(delta):
-	$Area2D.position += utils.DIRECTION_VECTOR[direction]*speed*delta
-
-func _on_Area2D_body_entered(body):
-	var collider = body.get_parent()
-	if collider.has_method("receiveDamage"):
-		collider.receiveDamage(0,0, $Area2D.global_position)
-	match collider.get_name():
-		"Player","Map", "EvilBlock":
-			direction = utils.oppositeDirection(direction)
-
-func _on_Area2D_area_shape_entered(area_id, area, area_shape, self_shape):
-	match area.get_parent().get_name().substr(0,9):
-		"EvilBlock":
-			direction = utils.oppositeDirection(direction)
+	var collision = $Kinematic2D.move_and_collide(utils.DIRECTION_VECTOR[direction]*speed*delta)
+	if collision:
+		var collider = collision.get_collider().get_parent()
+		if collider.has_method("receiveDamage"):
+			collider.receiveDamage(0,0, $Kinematic2D.global_position)
+		
+		match collider.get_name().substr(0,9):
+			"Player","Map", "EvilBlock":
+				direction = utils.oppositeDirection(direction)
